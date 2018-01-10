33c1a4bba6438b201995d10b31eef3b9d20b312f
Sasitha Rajapaksha <sir@sl.tiqri.com>
2018-01-10 10:35:44 +0530

    initial commit of Front end Angular project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component' 
+import { LoginComponent } from './login/login.component';
+import { CoursesComponent } from './courses/courses.component';
+import { DetailsComponent } from './details/details.component';
+import { EnrollComponent } from './enroll/enroll.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { CoursesComponent } from './courses/courses.component';
+import { DetailsComponent } from './details/details.component';
+import { EnrollComponent } from './enroll/enroll.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    CoursesComponent,
+    DetailsComponent,
+    EnrollComponent
   ],
   imports: [
     BrowserModule,
