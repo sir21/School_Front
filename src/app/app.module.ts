11522cb196304272ad67c3564e5546ca6dee4283
Sasitha Rajapaksha <sir@sl.tiqri.com>
2018-01-18 07:49:09 +0530

    Navbar created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -31,7 +32,8 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LoginComponent } from '../login/login.component';
+
+import { AppComponent } from './../app.component';
+import { logging } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
+export class HomeComponent implements OnInit {  
+  constructor(private httpClient:HttpClient) {}
 
   ngOnInit() {
+    this.httpClient.get('http://localhost:1816/api/Home/index')
+    .subscribe(
+      (data:any[]) =>{
+        console.log(data);
+      }
+    )
   }
 
 }
