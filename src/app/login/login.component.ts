9b73ce2ea14df8dfe395fb85ce78c8d197c31036
Sasitha Rajapaksha <sir@sl.tiqri.com>
2018-01-23 08:04:06 +0530

    Cors resolved and login redesigned

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { templateSourceUrl } from '@angular/compiler';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -16,9 +17,13 @@ export class LoginComponent implements OnInit {
   emailAlert: string = 'This field is requiered';
   passwordAlert: string = 'Password is required';
 
+  sucsessLogin: string = 'Login is a success';
+  failedLogin: string = 'Login is a Fail';
+  loginMessage: string = '';
+
   notFill: boolean = false;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private httpClient:HttpClient) {
     this.rForm = fb.group({
       'email': [null, Validators.compose([Validators.required, Validators.email])],
       'password': [null, Validators.required]
@@ -30,6 +35,29 @@ export class LoginComponent implements OnInit {
       this.notFill = true;
     else
       this.notFill = false;
+
+    if(!this.notFill)
+      this.loginCall(post);
+  }
+
+  loginCall(post) {
+    this.email = post.email;
+    this.password = post.password;
+
+    this.httpClient.post('http://localhost:1816/api/login/login', {
+      email: this.email,
+      password: this.password
+    })
+    .subscribe(
+      (data:any) => {
+        console.log(data);
+        this.loginMessage = this.sucsessLogin;
+      },
+      (err:any) => {
+        console.log(err);
+        this.loginMessage = this.failedLogin;
+      }
+    )
   }
 
   ngOnInit() {
